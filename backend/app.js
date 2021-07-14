c61309b1de5f283fed1238c087355261b912b0d3
Lea SOLO KWAN <leasolok@gmail.com>
2021-07-14 15:51:33 +0200

    Decouverte et mise en place de middlewares simples

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,29 @@ const express = require('express');
 
 const app = express();
 
-// Réponse simple pour éviter 404 et vérifier bon fonctionnement
-app.use((req, res) => {
-   res.json({ message: 'Votre requête a bien été reçue !' }); 
+// Premiers middlewares
+// req = request , res = response , next ( permet au middleware de paser l'éxecution au middleware suivant)
+app.use((req, res, next ) => {
+    console.log('Requête recue');
+    // Renvoie prochain middleware
+    next();
 });
 
+app.use((req, res, next ) => {
+    // Modification du code de la requête
+    res.status(201);
+    // Renvoie prochain middleware
+    next();
+});
+
+app.use((req, res, next) => {
+    // Envoie Réponse simple
+   res.json({ message: 'Votre requête a bien été reçue !' }); 
+   next();
+});
+
+// Dernier middleware  ( écrit dans la console ) pas besoin next
+app.use((req, res ) => {
+    console.log('Reponse envoyée dans précédent middleware ( ne peut plus être modif )');
+});
 module.exports = app;
